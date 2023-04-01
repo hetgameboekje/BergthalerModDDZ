b138e9ee90011a7ed21edbab95face2471480485
HetGameBoekje <bergthaler.timo@gmail.com>
2023-04-01 18:08:33 +0200

    Created crafting recipe for cactus planks

--- src/main/java/net/fabricmc/ddz/ddzMain.java
+++ src/main/java/net/fabricmc/ddz/ddzMain.java
@@ -6,7 +6,6 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
