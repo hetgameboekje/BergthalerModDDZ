ebad26cf073c781f4147d180ed097f5efaabe4fd
HetGameBoekje <bergthaler.timo@gmail.com>
2023-04-01 16:22:19 +0200

    Changed the item to block

--- src/main/java/net/fabricmc/ddz/ddzMain.java
+++ src/main/java/net/fabricmc/ddz/ddzMain.java
@@ -2,6 +2,10 @@ package net.fabricmc.ddz;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -13,14 +17,16 @@ public class 	ddzMain implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-	public static final Item CACTUS_PLANK = new Item(new FabricItemSettings());
+	public static final Logger LOGGER = LoggerFactory.getLogger("ddz");
+	public static final Block CACTUS_PLANK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registries.ITEM, new Identifier("ddz", "cactus_plank"), CACTUS_PLANK);
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.BLOCK, new Identifier("tutorial", "cactus_plank"), CACTUS_PLANK);
+		Registry.register(Registries.ITEM, new Identifier("tutorial", "cactus_plank"), new BlockItem(CACTUS_PLANK, new FabricItemSettings()));
+
 	}
 }
