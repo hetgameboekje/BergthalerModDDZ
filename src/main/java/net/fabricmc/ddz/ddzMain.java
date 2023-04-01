609f22f1f46fd3063b95df7fa6b67c74145fce2c
HetGameBoekje <bergthaler.timo@gmail.com>
2023-04-01 17:35:58 +0200

    Balanced to wood

--- src/main/java/net/fabricmc/ddz/ddzMain.java
+++ src/main/java/net/fabricmc/ddz/ddzMain.java
@@ -18,7 +18,7 @@ public class 	ddzMain implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("ddz");
-	public static final Block CACTUS_PLANK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+	public static final Block CACTUS_PLANK = new Block(FabricBlockSettings.of(Material.CACTUS).strength(1.0f));
 
 	@Override
 	public void onInitialize() {
